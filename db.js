7f4367e974010542de83de50f9d4b87af32f146e
Dharineesh <baludharan90@gmail.com>
2020-05-29 16:03:12 +0530

    commit

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 
-mongoose.connect(process.env.MONGO_URL,{ 
+mongoose.connect("mongodb+srv://goodtimes_pass:Passwordno1!@cluster0-ittto.mongodb.net/test?retryWrites=true&w=majority",{ 
     useNewUrlParser: true,
     useUnifiedTopology: true 
  });
