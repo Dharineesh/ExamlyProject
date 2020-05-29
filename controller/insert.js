ae0723a0de55db52fda0a3a909908c0ac03f6b5f
Dharineesh <baludharan90@gmail.com>
2020-05-29 11:54:38 +0530

    commit

--- controller/insert.js
+++ controller/insert.js
@@ -89,7 +89,7 @@ updateProfile.post('/',(req,res) => {
 
 showProfile.use(bodyparser.urlencoded({extended:true}));
 showProfile.get('/',(req,res) => {
-    email="dharan@gmail.com"
+    // email="dharan@gmail.com"
     function respond(result,proRes){
         var html = "<html>";
         html += "<head><link rel=\"stylesheet\" href=\"https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css\"><link rel=\"stylesheet\" href=\"https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css\" integrity=\"sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk\" crossorigin=\"anonymous\"></head><body>";
@@ -119,7 +119,7 @@ showProfile.get('/',(req,res) => {
 notify.use(bodyparser.urlencoded({extended:true}));
 notify.get('/',(req,res) => {
     const general = "generalToAll";
-    email = "dharan@gmail.com";
+    // email = "dharan@gmail.com";
     const gen = notificationData.findOne({email : general}).select();
     const completed = CompletedCourse.find
     global.comp = "";
